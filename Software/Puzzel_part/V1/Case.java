dc375833608205a95b851ffa9e285755a527cdc4
Xteck_r <robin.auriac@ensea.fr>
2023-05-18 21:28:57 +0200

    Base impleted

--- Software/Puzzel_part/V1/Case.java
+++ Software/Puzzel_part/V1/Case.java
@@ -7,26 +7,34 @@
 //============================================================================//
 
 
-private class Case{
-
+public class Case{
 
 //================================================= THIS IS ALL THE WAY TO BUILD A CASE ============================
 
 public Case(){
-  this.cordonner=[0,0,0];
+  //this.cordonner={0,0,0};   ne semble pas marcher
+  this.cordonner[0]=0;
+  this.cordonner[1]=0;
+  this.cordonner[2]=0;
   this.type=0;
 }
 public Case(int x, int y){
-  this.cordonner=[x,y,0];
+  this.cordonner[0]=x;
+  this.cordonner[1]=y;
+  this.cordonner[2]=0;
   this.type=0;
 }
 public Case(int x, int y,int z){
-  this.cordonner=[x,y,z];
+  this.cordonner[0]=x;
+  this.cordonner[1]=y;
+  this.cordonner[2]=z;
   this.type=0;
 }
-public Case(int x, int y,int z,int class){
-  this.cordonner=[x,y,z];
-  this.type=class;
+public Case(int x, int y,int z,int type){
+  this.cordonner[0]=x;
+  this.cordonner[1]=y;
+  this.cordonner[2]=z;
+  this.type=type;
 }
 
 public Case(int[2] posit,int class){
@@ -49,6 +57,7 @@ public PrintCase(){
 
 //================================================== THIS IS ALL THE ARGUMENT =====================================
 
-private int[2] cordonner;
+public int[] cordonner;
 private int type; // this is the tape of the ground
+
 }
