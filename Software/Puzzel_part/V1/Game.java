4bb6c0c38085b509f74d244663d53af3e37a4ca9
Xteck_r <robin.auriac@ensea.fr>
2023-05-18 15:40:41 +0200

    new fonc

---
+++ Software/Puzzel_part/V1/Game.java
@@ -0,0 +1,16 @@
+public class Game{
+
+public Game(){
+  This.NameGame="FE_Like";
+
+
+}
+
+public End_Game(){}
+public Call_Menu(){}
+
+private String NameGame;
+private Turn MomentGame;
+private Player[] PlayerInGame;
+
+}
